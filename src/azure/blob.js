ccf414eec9c1d3144991dd1a985d2106529b078f
Rudolf <rudolf_bast@live.com>
2017-12-30 11:18:11 +0700

    Update azure blob module to use stream instead of text

--- src/azure/blob.js
+++ src/azure/blob.js
@@ -7,14 +7,18 @@ const config = require(__dirname + '/../../config.json');
 
 const blobService = azure.createBlobService(config.azure.account, config.azure.storage_access_key);
 
-function uploadBlobFromText(content, name, callback) {
-    blobService.createBlockBlobFromText(config.azure.container, name, content, function(err, result, response) {
-        if (!err) {
-            if (_.isFunction(callback)) {
-                callback(blobService.getUrl(result.container, result.name));
-            }
+function uploadBlobFromStream(content, name, callback) {
+    content.pipe(blobService.createWriteStreamToBlockBlob(config.azure.container, name, (err, result) => {
+        if (err) {
+            throw err;
         }
-    });
+
+        if (_.isFunction(callback)) {
+            callback(blobService.getUrl(config.azure.container, name));
+        }
+    }));
 }
 
-module.exports = uploadBlobFromText;
+module.exports = {
+    uploadBlobFromStream: uploadBlobFromStream
+};
